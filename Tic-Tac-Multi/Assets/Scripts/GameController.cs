630a5419ae4ebb861da3ae364134c366916c1bb5
j <j>
2021-03-23 11:36:06 -0400

    Balloon pop

--- Tic-Tac-Multi/Assets/Scripts/GameController.cs
+++ Tic-Tac-Multi/Assets/Scripts/GameController.cs
@@ -8,22 +8,16 @@ public class GameController : MonoBehaviour
 
     public Text[] buttonList;
     private string playerSide;
+    public GameObject gameOverPanel;
+    public Text gameOverText;
+    private int moveCount;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
 void Awake () {
      SetGameControllerReferenceOnButtons(); 
      playerSide = "X";
+     gameOverPanel.SetActive(false);
+     moveCount = 0;
      }
 
 void SetGameControllerReferenceOnButtons () {
@@ -37,6 +31,8 @@ public string GetPlayerSide () {
      } 
 
 public void EndTurn () {
+
+    moveCount++;
      if (buttonList [0].text == playerSide && buttonList [1].text == playerSide && buttonList [2].text == playerSide) {
          GameOver();
          }
@@ -68,14 +64,48 @@ public void EndTurn () {
         if (buttonList [2].text == playerSide && buttonList [4].text == playerSide && buttonList [6].text == playerSide)        {
             GameOver();
         }
+
+        if (moveCount >= 9)
+        {
+            SetGameOverText ("It's a draw!");
+        }
+        
+        ChangeSides();
         
      }
 
 void GameOver () {
-    for (int i = 0; i < buttonList.Length; i++) {
-        buttonList[i].GetComponentInParent<Button>().interactable = false;
-    }
+    SetBoardInteractable(false);
+    SetGameOverText (playerSide + " Wins!");
 }
+
+void ChangeSides ()
+    {
+        playerSide = (playerSide == "X") ? "O" : "X";
+    }
+
+void SetGameOverText (string value)
+    {
+        gameOverPanel.SetActive(true);
+        gameOverText.text = value;
+    }
+
+public void RestartGame() {
+    playerSide = "X";
+    moveCount = 0;
+    gameOverPanel.SetActive(false);
+    SetBoardInteractable(true);
+
+    for (int i = 0; i < buttonList.Length; i++){
+        buttonList [i].text = "";
+        }
+    }
+
+void SetBoardInteractable (bool toggle) {
+    for (int i = 0; i < buttonList.Length; i++) {
+         buttonList[i].GetComponentInParent<Button>().interactable = toggle; 
+         }
+    }
 
 
 }
